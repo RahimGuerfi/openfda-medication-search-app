504b7571619b5bb22116a84bbdf16bead7cdaac2
RahimGuerfi <89612748+RahimGuerfi@users.noreply.github.com>
2024-06-19 23:00:49 +0100

    feat: imrpove medication details layout

--- src/components/MedicationDetails.tsx
+++ src/components/MedicationDetails.tsx
@@ -1,5 +1,13 @@
 import { IMedication } from "@/types";
-import { Box, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { MedicationDetailsAccordion } from "./MedicationDetailsAccordion";
 
 type Props = {
@@ -9,83 +17,116 @@ type Props = {
 export const MedicationDetails = ({ medication }: Props) => {
   return (
     <Container maxWidth="lg" sx={{ py: { xs: 8, sm: 16 } }}>
-      <Typography
-        textAlign="center"
-        variant="h2"
-        sx={{
-          color: "primary.main",
-        }}
-        gutterBottom
-      >
-        {medication.openfda.brand_name?.[0] ||
-          medication.openfda.generic_name?.[0] ||
-          "Unknown Medication"}
-      </Typography>
+      <Stack flexDirection="column" gap={2}>
+        <Card>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="body1" fontWeight="500">
+                  Brand Name:
+                </Typography>
+                <Typography variant="body2">
+                  {medication.openfda.brand_name?.join(", ") || "Unknown"}
+                </Typography>
+              </Grid>
 
-      <Typography variant="subtitle2" textAlign="center" gutterBottom>
-        Manufacturer: {medication.openfda.manufacturer_name?.[0] || "Unknown"}
-      </Typography>
-      <Typography variant="subtitle2" textAlign="center" gutterBottom>
-        Application Number:{" "}
-        {medication.openfda.application_number?.[0] || "N/A"}
-      </Typography>
-      <Typography variant="subtitle2" textAlign="center" gutterBottom>
-        Route: {medication.openfda.route?.join(", ") || "N/A"}
-      </Typography>
-      <Typography variant="subtitle2" textAlign="center" gutterBottom>
-        Substance Name: {medication.openfda.substance_name?.join(", ") || "N/A"}
-      </Typography>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="body1" fontWeight="500">
+                  Generic Name:
+                </Typography>
+                <Typography variant="body2">
+                  {medication.openfda.generic_name?.join(", ") || "Unknown"}
+                </Typography>
+              </Grid>
 
-      <Box
-        sx={{
-          p: 2,
-        }}
-      >
-        <MedicationDetailsAccordion
-          title="Product Data elements"
-          data={medication.spl_product_data_elements}
-        />
-        <MedicationDetailsAccordion
-          title="Indications and Usage"
-          data={medication.indications_and_usage}
-        />
-        <MedicationDetailsAccordion
-          title="Dosage and Administration"
-          data={medication.dosage_and_administration}
-        />
-        <MedicationDetailsAccordion
-          title="Warnings and Cautions"
-          data={medication.warnings_and_cautions}
-        />
-        <MedicationDetailsAccordion
-          title="Adverse Reactions"
-          data={medication.adverse_reactions}
-        />
-        <MedicationDetailsAccordion
-          title="Drug Interactions"
-          data={medication.drug_interactions}
-        />
-        <MedicationDetailsAccordion
-          title="Use in Specific Populations"
-          data={medication.use_in_specific_populations}
-        />
-        <MedicationDetailsAccordion
-          title="Description"
-          data={medication.description}
-        />
-        <MedicationDetailsAccordion
-          title="Clinical Pharmacology"
-          data={medication.clinical_pharmacology}
-        />
-        <MedicationDetailsAccordion
-          title="How Supplied"
-          data={medication.how_supplied}
-        />
-        <MedicationDetailsAccordion
-          title="Precautions"
-          data={medication.precautions}
-        />
-      </Box>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="body1" fontWeight="500">
+                  Manufacturer:
+                </Typography>
+                <Typography variant="body2">
+                  {medication.openfda.manufacturer_name?.join(", ") ||
+                    "Unknown"}
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Typography variant="body1" fontWeight="500">
+                  Application Number:
+                </Typography>
+                <Typography variant="body2">
+                  {medication.openfda.application_number?.join(", ") ||
+                    "Unknown"}
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Typography variant="body1" fontWeight="500">
+                  Route:
+                </Typography>
+                <Typography variant="body2">
+                  {medication.openfda.route?.join(", ") || "Unknown"}
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Typography variant="body1" fontWeight="500">
+                  Substance Name:
+                </Typography>
+                <Typography variant="body2">
+                  {medication.openfda.substance_name?.join(", ") || "Unknown"}
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+
+        <Box>
+          <MedicationDetailsAccordion
+            title="Product Data elements"
+            data={medication.spl_product_data_elements}
+          />
+          <MedicationDetailsAccordion
+            title="Indications and Usage"
+            data={medication.indications_and_usage}
+          />
+          <MedicationDetailsAccordion
+            title="Dosage and Administration"
+            data={medication.dosage_and_administration}
+          />
+          <MedicationDetailsAccordion
+            title="Warnings and Cautions"
+            data={medication.warnings_and_cautions}
+          />
+          <MedicationDetailsAccordion
+            title="Adverse Reactions"
+            data={medication.adverse_reactions}
+          />
+          <MedicationDetailsAccordion
+            title="Drug Interactions"
+            data={medication.drug_interactions}
+          />
+          <MedicationDetailsAccordion
+            title="Use in Specific Populations"
+            data={medication.use_in_specific_populations}
+          />
+          <MedicationDetailsAccordion
+            title="Description"
+            data={medication.description}
+          />
+          <MedicationDetailsAccordion
+            title="Clinical Pharmacology"
+            data={medication.clinical_pharmacology}
+          />
+          <MedicationDetailsAccordion
+            title="How Supplied"
+            data={medication.how_supplied}
+          />
+          <MedicationDetailsAccordion
+            title="Precautions"
+            data={medication.precautions}
+          />
+        </Box>
+      </Stack>
     </Container>
   );
 };
